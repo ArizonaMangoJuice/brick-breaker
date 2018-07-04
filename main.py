4b98be6fd78b1d6fa30bbcf91452eb545db3e963
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-04 09:52:16 -0600

    now starting to add gameOver widget

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import NumericProperty, ReferenceListProperty,\
     ObjectProperty
 from kivy.vector import Vector
@@ -28,6 +30,7 @@ class Paddle(Widget):
             bounced = Vector(-1 * -8.574355240000006, 0.48346681823204063)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
+            self.score += 1
 
 class Ball(Widget):
     velocity_x = NumericProperty(0)
@@ -47,7 +50,38 @@ class Game(Widget):
     brick4 = ObjectProperty(None)
     brick5 = ObjectProperty(None)
     brick6 = ObjectProperty(None)
-    # player2 = ObjectProperty(None)
+    brick7 = ObjectProperty(None)
+    brick8 = ObjectProperty(None)
+    brick9 = ObjectProperty(None)
+    brick10 = ObjectProperty(None)
+
+    def test(self):
+        layout = BoxLayout(orientation='vertical')
+        b1 = Button(text='button 1')
+        b2 = Button(text='button 2')
+        layout.add_widget(b1)
+        layout.add_widget(b2)
+        return layout
+
+    def start(self):
+        Clock.unschedule(self.update)
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+
+    def stop(self):
+        # Stop updating
+        Clock.unschedule(self.update)    
+
+    def reset(self):
+        self.brick1.x = self.width - 35
+        self.brick2.x = self.width - 35
+        self.brick3.x = self.width - 35
+        self.brick4.x = self.width - 85
+        self.brick5.x = self.width - 85
+        self.brick6.x = self.width - 85
+        self.brick7.x = self.width - 135
+        self.brick8.x = self.width - 135
+        self.brick9.x = self.width - 135
+        self.brick10.x = self.width - 185
 
     def serve_ball(self, vel=(-8.574355240000006, 0.48346681823204063)):
         # print(self.ball.center)
@@ -71,7 +105,23 @@ class Game(Widget):
         self.brick9.hitBrick(self.ball)
         self.brick10.hitBrick(self.ball)
         # self.player2.bounce_ball(self.ball)
-
+        if (
+            self.brick10.x == -150 and
+            self.brick9.x == -150 and 
+            self.brick8.x == -150 and
+            self.brick7.x == -150 and
+            self.brick6.x == -150 and
+            self.brick5.x == -150 and
+            self.brick4.x == -150 and
+            self.brick3.x == -150 and
+            self.brick2.x == -150 and
+            self.brick1.x == -150
+           ):
+            self.stop()
+            self.serve_ball(vel=(4, 0))
+            self.reset()
+            self.start()
+        
         # bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
@@ -84,16 +134,7 @@ class Game(Widget):
             # self.player2.score += 1
             self.serve_ball(vel=(4, 0))
             print(self.brick1.x)
-            self.brick1.x = self.width - 35
-            self.brick2.x = self.width - 35
-            self.brick3.x = self.width - 35
-            self.brick4.x = self.width - 85
-            self.brick5.x = self.width - 85
-            self.brick6.x = self.width - 85
-            self.brick7.x = self.width - 135
-            self.brick8.x = self.width - 135
-            self.brick9.x = self.width - 135
-            self.brick10.x = self.width - 185
+            self.reset()
         # if self.ball.x > self.width:
         #     self.player1.score += 1
         #     self.serve_ball(vel=(-4, 0))
@@ -109,7 +150,8 @@ class brickBreaker(App):
     def build(self):
         game = Game()
         game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        game.start()
+        
         return game
 
 
