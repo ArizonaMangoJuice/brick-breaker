23462065d204b7096693ea6b413a58da354dd0e9
Isael Lizama <isaelwebdeveloper@gmail.com>
2018-07-06 07:13:38 -0600

    added comments to main

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 from kivy.graphics import Color
+
+# class for making a brick in the game
 class Brick(Widget):
+    # creates hit detection for when the ball hits the brick
     def hitBrick(self, ball):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
@@ -21,33 +24,33 @@ class Brick(Widget):
             print(self.x)
             self.x = -150
             
-
+# class for making the paddle in the game
 class Paddle(Widget):
-    score = NumericProperty(0)
-
+    # creates hit detection for when the ball hits the paddle
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            # vx, vy = [-8.574355240000006, 0.48346681823204063]
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * -8.574355240000006, 0.48346681823204063)
             vel = bounced * 1.1
             ball.velocity = vel.x, vel.y + offset
-            self.score += 1
 
+
+# class for creating the ball in the game
 class Ball(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
+    # method to move the ball
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
 
-
+# class for creating the Game
 class Game(Widget):
+    # create the ball player and the bricks
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     brick1 = ObjectProperty(None)
-    # brick1.Color(1, 1, 1, 1)
     brick2 = ObjectProperty(None)
     brick3 = ObjectProperty(None)
     brick4 = ObjectProperty(None)
@@ -58,24 +61,27 @@ class Game(Widget):
     brick9 = ObjectProperty(None)
     brick10 = ObjectProperty(None)
     test = Button(text='')
+
+    # When the class get called start method will run first
     def __init__(self, *args, **kwargs):
         super(Game, self).__init__(*args, **kwargs)
-        # self.test.bind(on_release = self.serve_ball(vel=(4, 0))
         self.start()
         
-
+    # hides the gameOver button when the player clicks restart
     def hideRestart(self):
         self.start()
         self.test.x = -500
 
+    # starts the clock to let the game run
     def start(self):
         Clock.unschedule(self.update)
         Clock.schedule_interval(self.update, 1.0 / 60.0)
 
+    # stops the clock to pause the game
     def stop(self):
-        # Stop updating
         Clock.unschedule(self.update)    
 
+    # resets all bricks back to original spot
     def reset(self):
         self.brick1.x = self.width - 35
         self.brick2.x = self.width - 35
@@ -88,15 +94,18 @@ class Game(Widget):
         self.brick9.x = self.width - 135
         self.brick10.x = self.width - 185
 
+    # method for the ball speed and placement
     def serve_ball(self, vel=(-8.574355240000006, 0.48346681823204063)):
         self.ball.center = [104.0, 300]
-        print(vel)
         self.ball.velocity = vel
 
+    # main method that runs when the clock starts
     def update(self, dt):
+        # start the ball 
         self.ball.move()
-
+        # initialize the method for the paddle
         self.player1.bounce_ball(self.ball)
+        # initialize the method for the bricks
         self.brick1.hitBrick(self.ball)
         self.brick2.hitBrick(self.ball)
         self.brick3.hitBrick(self.ball)
@@ -107,6 +116,7 @@ class Game(Widget):
         self.brick8.hitBrick(self.ball)
         self.brick9.hitBrick(self.ball)
         self.brick10.hitBrick(self.ball)
+        # if all the bricks are hit
         if (
             self.brick10.x == -150 and 
             self.brick9.x == -150 and 
@@ -122,39 +132,39 @@ class Game(Widget):
             self.stop()
             self.serve_ball(vel=(4, 0))
             self.reset()
-           
-            # self.test.text = 'game Over'
-            print(self.parent.height)
             self.test.center_x = self.parent.height / 2
             self.test.center_y = self.parent.height / 2
-            # self.test.on_release =  self.serve_ball(vel=(4, 0))
-            print('test x',self.test.center)
-            
-            # self.start()
         
+        # if the ball hits the top or bottom of the screen bounce the ball
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1
 
+        # if the ball hits the right side of the screen bounce
         if (self.ball.x < 0) or (self.ball.right > self.width):
             self.ball.velocity_x *= -1
 
+        # if the ball hits the left side of the screen reset
         if self.ball.x < self.x:
             self.serve_ball(vel=(4, 0))
             print(self.brick1.x)
             self.reset()
 
+    # allows you to move the paddle by clicking it
     def on_touch_move(self, touch):
         if touch.x < self.width / 3:
             self.player1.center_y = touch.y
 
-
+# class that lets you have screens for the app
 class Manager(ScreenManager):
     pass
 
+# root Widget 
 class ScreensApp(App):
+    # build methods get detected by kivy and runs first
     def build(self):
-        root = self.root
+        # load the kivy file
         self.load_kv('brickBreaker.kv')
+        # when the user clicks the button doa WipeTransition
         return Manager(transition=WipeTransition())
 
 if __name__ == '__main__':
